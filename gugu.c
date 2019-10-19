482de7c42db19a756856b3243a27c5ff7d888090
donghoon <sin40dh73@gmail.com>
2019-10-19 19:30:38 +0900

    gugu.c

---
+++ gugu.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void){
+  for(int i = 1; i < 10; i++){
+    for(int d = 1; d < 10; d++){
+      printf("%3d X %2d = %3d",d,i,d*i);
+    } printf("\n");
+  }
+  return 0;
+}
